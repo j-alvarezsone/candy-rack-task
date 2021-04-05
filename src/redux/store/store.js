979243f204cb367576246a190875b2aedcec41fd
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-05 23:39:21 +0200

    changed redux middleware for production

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -7,8 +7,14 @@ import { OffersReducer } from '../offers/reducer';
 const composeEnhancers =
   (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-const middlewareList = [thunk, logger];
+let middleware = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middleware = [...middleware, thunk, logger];
+} else {
+  middleware = [...middleware, thunk];
+}
 
 const reducers = combineReducers({ data: OffersReducer, cart: cartReducer });
 
-export const store = createStore(reducers, composeEnhancers(applyMiddleware(...middlewareList)));
+export const store = createStore(reducers, composeEnhancers(applyMiddleware(...middleware)));
