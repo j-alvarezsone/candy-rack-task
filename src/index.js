45046b31f5ef7f1d5e66e19a6177bcf7482af384
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-15 09:37:28 +0200

    removed unnecessary css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CandyRack } from './CandyRack';
 import './index.css';
-import './index.css';
 
 ReactDOM.render(<CandyRack />, document.getElementById('root'));
